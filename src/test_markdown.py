79bbe5373f47d4678a0db017849a82f73c2dbcc3
jaakkv <jaakko.vaaramaki@gmail.com>
2026-03-20 20:43:49 +0200

    G: Added markdown.py and tests for it

---
+++ src/test_markdown.py
@@ -0,0 +1,74 @@
+import unittest
+from textnode import TextNode, TextType
+from markdown import split_nodes_delimiter
+
+
+class TestInlineMarkdown(unittest.TestCase):
+    def test_delim_bold(self):
+        node = TextNode("This is text with a **bolded** word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("bolded", TextType.BOLD),
+                TextNode(" word", TextType.TEXT),
+            ],
+            new_nodes,
+        )
+
+    def test_delim_bold_double(self):
+        node = TextNode(
+            "This is text with a **bolded** word and **another**", TextType.TEXT
+        )
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("bolded", TextType.BOLD),
+                TextNode(" word and ", TextType.TEXT),
+                TextNode("another", TextType.BOLD),
+            ],
+            new_nodes,
+        )
+
+    def test_delim_italic(self):
+        node = TextNode("This is text with an *italic* word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "*", TextType.ITALIC)
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word", TextType.TEXT),
+            ],
+            new_nodes,
+        )
+
+    def test_delim_code(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" word", TextType.TEXT),
+            ],
+            new_nodes,
+        )
+
+    def test_unmatched_delimiter(self):
+        node = TextNode("This has an unmatched `code block", TextType.TEXT)
+        with self.assertRaises(ValueError):
+            split_nodes_delimiter([node], "`", TextType.CODE)
+
+    def test_ignores_other_node_types(self):
+        nodes = [
+            TextNode("This is bold", TextType.BOLD),
+            TextNode("This is normal", TextType.TEXT)
+        ]
+        new_nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+        self.assertEqual(len(new_nodes), 2)
+        self.assertEqual(new_nodes[0].text_type, TextType.BOLD)
+
+
+if __name__ == "__main__":
+    unittest.main()
