04afd5137fdd848a18005b8ac2b029ff5b2ebf62
jaakkv <jaakko.vaaramaki@gmail.com>
2026-03-23 16:58:13 +0200

    H: Added two new functions to markdown.p and test cases for them

--- src/test_markdown.py
+++ src/test_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 from textnode import TextNode, TextType
-from markdown import split_nodes_delimiter
+from markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
 
 class TestInlineMarkdown(unittest.TestCase):
@@ -69,6 +69,47 @@ class TestInlineMarkdown(unittest.TestCase):
         self.assertEqual(len(new_nodes), 2)
         self.assertEqual(new_nodes[0].text_type, TextType.BOLD)
 
+class TestMarkdownExtractors(unittest.TestCase):
+    # --- Image Extraction Tests ---
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_images_multiple(self):
+        text = "Here's a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        matches = extract_markdown_images(text)
+        self.assertListEqual(
+            [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")],
+            matches
+        )
+
+    def test_extract_markdown_images_no_match(self):
+        text = "This is text with a regular [link](https://boot.dev) only."
+        matches = extract_markdown_images(text)
+        self.assertListEqual([], matches)
+
+    # --- Link Extraction Tests ---
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev)"
+        )
+        self.assertListEqual([("to boot dev", "https://www.boot.dev")], matches)
+
+    def test_extract_markdown_links_multiple(self):
+        text = "Links [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        matches = extract_markdown_links(text)
+        self.assertListEqual(
+            [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")],
+            matches
+        )
+
+    def test_extract_markdown_links_ignores_images(self):
+        """Crucial test: ensures links regex doesn't catch image syntax."""
+        text = "This has an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://boot.dev)"
+        matches = extract_markdown_links(text)
+        self.assertListEqual([("link", "https://boot.dev")], matches)
 
 if __name__ == "__main__":
     unittest.main()
